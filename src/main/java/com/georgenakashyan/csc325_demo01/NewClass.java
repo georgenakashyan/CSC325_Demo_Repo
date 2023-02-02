dee231f682d3bdc96ab697d992c2dc88c5d58dbb
main <GeorgeNakashyan@gmail.com>
2023-02-02 00:58:52 -0500

    Added age, setters, getters, and constructors

--- src/main/java/com/georgenakashyan/csc325_demo01/NewClass.java
+++ src/main/java/com/georgenakashyan/csc325_demo01/NewClass.java
@@ -9,5 +9,21 @@ package com.georgenakashyan.csc325_demo01;
  * @author main
  */
 public class NewClass {
-    
+
+    private int age;
+        
+    public NewClass() {
+    }
+
+    public NewClass(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
